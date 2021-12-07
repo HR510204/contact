7e2372fd9a9c818eb9f3cf58635d341d12ded087
abhinav <abhinav.sharma@techchefz.com>
2021-12-07 18:59:10 +0530

    initial setup

---
+++ components/contactUs/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Styles from "../../styles/contactUs/contact.module.scss";
+const index = () => {
+  return <div className={Styles.container}>contact us</div>;
+};
+
+export default index;
